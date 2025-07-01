e7d9ff33785085e335b7c733e099a78cab6a180b
Rabi Kishan Rauniyar <rabikishan.official@gmail.com>
2025-07-01 17:43:04 +0545

    X657b: update

--- twrp_x657B.mk
+++ twrp_x657B.mk
@@ -10,16 +10,16 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from Infinix-X657B device
-$(call inherit-product, device/infinix/Infinix-X657B/device.mk)
+# Inherit from x657B device
+$(call inherit-product, device/infinix/x657B/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Infinix-X657B
-PRODUCT_NAME := omni_Infinix-X657B
+PRODUCT_DEVICE := x657B
+PRODUCT_NAME := twrp_x657B
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix X657B
+PRODUCT_MODEL := Infinix x657B
 PRODUCT_MANUFACTURER := infinix
