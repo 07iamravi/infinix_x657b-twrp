65b98d49eb6d033428bd04fef31ddc5749b761d8
Rabi Kishan Rauniyar <rabikishan.official@gmail.com>
2025-07-01 17:50:04 +0545

    x657B: removed unwanted flags and added some flags for stability

--- twrp_x657B.mk
+++ twrp_x657B.mk
@@ -6,12 +6,10 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit from x657B device
-$(call inherit-product, device/infinix/x657B/device.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -21,5 +19,19 @@ $(call inherit-product, vendor/twrp/config/gsm.mk)
 PRODUCT_DEVICE := x657B
 PRODUCT_NAME := twrp_x657B
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix x657B
+PRODUCT_MODEL := x657B
 PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock
+
+# Bypass anti-rollback ROMs protection
+# Set build date to Jan 1 2009 00:00:00
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.date.utc=1230768000
