17f58ae397f7aa861769b8d65841ccc3d212da3f
Rabi Kishan Rauniyar <rabikishan.official@gmail.com>
2025-07-02 16:49:28 +0545

    x657b: init for omni

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,9 @@
 #
 # Copyright (C) 2021 The Android Open Source Project
-# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2021 SebaUbuntu's omni device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_x657B.mk
+    $(LOCAL_DIR)/omni_x657B.mk
